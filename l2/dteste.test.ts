c3f9938a3c35aafde110cdce8a4b7ab5bcad7d30
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-11-17 15:35:36 -0300

    teste varias operações

--- l2/dteste.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="dteste" project="102008" enhancement="_blank" folder="" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
