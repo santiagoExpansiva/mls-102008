11b30abfb10437fd34c6fb8eec9408443d84d309
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-11-17 14:17:04 -0300

    l2/eteste.test.ts

---
+++ l2/eteste.test.ts
@@ -0,0 +1,5 @@
+/// <mls shortName="eteste" project="102008" enhancement="_blank" folder="" />
+
+ import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
+ export const integrations: ICANIntegration[] = [];
+ export const tests: ICANTest[] = [];
